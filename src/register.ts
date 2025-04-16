81c96ebe8e330d33d81b27f4226404db99d92f5b
Lazcano007 <Pedrolazcano@live.com>
2025-04-16 18:40:22 +0200

    created register.ts and later added comments

--- src/register.ts
+++ src/register.ts
@@ -1,32 +1,33 @@
 import './style.scss';
 
-const form = document.getElementById("registerForm") as HTMLFormElement;
+const form = document.getElementById("registerForm") as HTMLFormElement;  // hämtar register-formuläret från DOM:en
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
 
+form.addEventListener("submit", (event) => {      // event är ett "event-objekt" som innehåller information om den händelsen som precis inträffade (submit i detta fall)
+  event.preventDefault();                         // förhindrar att sidan laddas om vid formulärinlämning
+
+    // hämtar värden från inputfälten
   const name = (document.getElementById("name") as HTMLInputElement).value;
   const email = (document.getElementById("email") as HTMLInputElement).value;
   const password = (document.getElementById("password") as HTMLInputElement).value;
 
-  try {
-    const response = await fetch(
-      "https://u05.onrender.com/api/v1/Users/registration",
-      {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ name, email, password }),
+  fetch("https://u05.onrender.com/api/v1/Users/registration", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ name, email, password }),
+  })
+    .then((response) => {
+      return response.json().then((data) => {
+        if (!response.ok) {
+          alert(data.message || "Registration failed, try again!");
+          return;
+        }
+        alert("Your account has been created!");
+        form.reset();                              // rensar formuläret
       });
-
-    const data = await response.json();
-    if (!response.ok) {
-      alert(data.message || "Registration failed, try again!");
-      return;
-    }
-    alert("Your account has been created!");
-    form.reset();
-  } catch (error) {
-    console.error("Error:", error);
-    alert("Unfortunately something went wrong!");
-  }
+    })
+    .catch((error) => {
+      console.error("Error creating an account", error);
+      alert("Unfortunately something went wrong, please try again later!");
+    });
 });
