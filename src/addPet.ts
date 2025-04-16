295574c6f3e6e8da420fe9dbcea84efb67f071a5
Lazcano007 <Pedrolazcano@live.com>
2025-04-16 18:38:49 +0200

    created addPet.ts  and later added comments

--- src/addPet.ts
+++ src/addPet.ts
@@ -1,35 +1,40 @@
-import './style.scss';
+import "./style.scss";
 
-const form = document.getElementById("addPetForm") as HTMLFormElement;
+const form = document.getElementById("addPetForm") as HTMLFormElement; // hämtar addPet-formuläret från DOM:en
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+form.addEventListener("submit", (event) => {           // event är ett "event-objekt" som innehåller information om den händelsen som precis inträffade (submit i detta fall)
+  event.preventDefault();                              // förhindrar att sidan laddas om vid formulärinlämning
 
-  const owner = localStorage.getItem("userId");
+  const owner = localStorage.getItem("userId");        // hämtar userId från localStorage
   if (!owner) {
-    alert("You must be logged in to add a pet!");
+    alert("You must be logged in to add a pet!"); 
     return;
   }
+   // hämtar värden från inputfälten
   const name = (document.getElementById("name") as HTMLInputElement).value;
   const species = (document.getElementById("species") as HTMLInputElement).value;
   const breed = (document.getElementById("breed") as HTMLInputElement).value;
   const age = Number((document.getElementById("age") as HTMLInputElement).value);
-  try {
-    const response = await fetch("https://u05.onrender.com/api/v1/Pets/create", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ owner, name, species, breed, age }),
+
+  fetch("https://u05.onrender.com/api/v1/Pets/create", { 
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ owner, name, species, breed, age }),
+  })
+    .then((response) => {
+      return response.json().then((data) => {
+        if (!response.ok) {
+          alert(
+            data.message || "Something went wrong when registering your pet!");
+          return;
+        }
+        alert("Your pet has successfully been added!");
+        form.reset();                               // rensar formuläret
+        window.location.href = "/dashboard.html";   // omdirigerar till dashboard-sidan
+      });
+    })
+    .catch((error) => {
+      console.error("Add pet error", error);        // hanterar nätverksfel eller andra oväntade fel
+      alert("Unfortunatelly something went wrong, try again later!");
     });
-    const data = await response.json();
-    if (!response.ok) {
-      alert(data.message || "Something went wrong when registering the pet.");
-      return;
-    }
-    alert("Your pet has successfully been added!");
-    form.reset();
-    window.location.href = "/dashboard.html";
-  } catch (error) {
-    console.error("Add pet error:", error);
-    alert("Unfortunately something went wrong.");
-  }
 });
