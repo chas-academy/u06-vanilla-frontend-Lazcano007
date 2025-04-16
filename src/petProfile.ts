614ee8b7f0373d0b211ed82353b69a253eaac373
Lazcano007 <Pedrolazcano@live.com>
2025-04-16 18:41:01 +0200

    added "span"tags for better design

--- src/petProfile.ts
+++ src/petProfile.ts
@@ -1,11 +1,9 @@
 import './style.scss';
 
-
 function getQueryParam(param: string): string | null {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(param);
 }
-
 
 const petId = getQueryParam("id");
 const petDetailsDiv = document.getElementById("petCard");
@@ -13,31 +11,38 @@ const petDetailsDiv = document.getElementById("petCard");
 if (!petId) {
   alert("No pet ID provided in the URL!");
 } else {
-
   fetch(`https://u05.onrender.com/api/v1/Pets/getPetById/${petId}`)
     .then((res) => {
       if (!res.ok) throw new Error("Response not ok");
       return res.json();
     })
     .then((pet) => {
-  
       const nameElement = document.getElementById("petName");
       if (nameElement) {
-        nameElement.textContent = pet.name;
+        nameElement.textContent = "Pet Profil";
       }
 
       if (petDetailsDiv) {
         petDetailsDiv.innerHTML = `
-          <h2>Pet Info</h2>
-          <p>Pet: ${pet.name}</p>
-          <p>Species: ${pet.species}</p>
-          <p>Breed: ${pet.breed}</p>
-          <p>Age: ${pet.age}</p>
-        `;
+        <div class="petFrame">
+          <button class="edit-btn" id="editPetBtn">Edit</button>
+          <h3></h3>
+          <div class="field"><span class="label">Name:</span> ${pet.name}</div>
+          <div class="field"><span class="label">Species:</span> ${pet.species}</div>
+          <div class="field"><span class="label">Breed:</span> ${pet.breed}</div>
+          <div class="field"><span class="label">Age:</span> ${pet.age}</div>
+        </div>`;
+
+        const editButton = document.getElementById("editPetBtn");
+        if (editButton) {
+          editButton.addEventListener("click", () => {
+            window.location.href = `/petEdit.html?id=${petId}`;
+          });
+        }
       }
     })
     .catch((error) => {
-      console.error("Error fetching pet details:", error);
+      console.error("There's been an error fetching your pet details:", error);
       if (petDetailsDiv) {
         petDetailsDiv.innerHTML = `<p>Error fetching pet details!</p>`;
       }
